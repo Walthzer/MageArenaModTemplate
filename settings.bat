3909ab4543dec994d92a14f912b5c21d80d5ac27
Walthzer <19926813+Walthzer@users.noreply.github.com>
2025-08-11 13:18:42 +0200

    push bat script

--- settings.bat
+++ settings.bat
@@ -2,11 +2,12 @@
 ::Before running any scripts ENSURE you have changed these variables correctly
 
 ::Change this to match the name of your Mod. It sets the name in Thunderstore and the release zip file.
-set MOD_NAME="MageArenaModTemplate"
+::Cannot contain spaces or special characters, no ' or " either!
+set MOD_NAME=text
 :: Change this your your anme as the author
-set MOD_AUTHOR="Walthzer"
+set MOD_AUTHOR=Walthzer
 ::Change this to your mods version, it will be set automatically into the `manifest.json` and the release zip
-set MOD_VERSION="1.0.0"
+set MOD_VERSION=1.0.0
 :: If you are using the Thunderstore local mod approach to start Mage Arena with your mod
 :: And want the build.bat script to automatically update your mod after every build
 :: set this to the path of your local mod in the correct profile
--- tools/setup.bat
+++ tools/setup.bat
@@ -1,11 +1,19 @@
 :: This script will do the initial setup for your mod.
 @echo off
 IF NOT EXIST "settings.bat" (
-    exit /b 1
+    exit 1
 )
+echo %MOD_NAME% | findstr /r /i ^^[a-z][a-z0-9-]*$ > nul && (
+    echo MOD_NAME has invalid characters. 
+    exit 1
+)
+
 call settings.bat
 :: setup the thunderstore manifest.json
+echo Setup packageFiles\manifest.json
 powershell -Command "((gc 'packageFiles\manifest.json') -replace 'MOD_NAME', '%MOD_NAME%') -replace 'MOD_AUTHOR', '%MOD_AUTHOR%' | Out-File -encoding ASCII 'packageFiles\manifest.json'"
 
 ::setup the project.csproj
-powershell -Command "(gc 'src\MageArenaModTemplate.csproj') -replace '(<AssemblyName>|<Product>)(.*?)(</AssemblyName>|</Product>)', '$1%MOD_NAME%$3' | Out-File -encoding ASCII 'src\MageArenaModTemplate.csproj'"
+echo Setup src\%MOd_NAME%.csproj
+powershell -Command "(gc 'src\project.csproj') -replace '(<AssemblyName>|<Product>)(.*?)(</AssemblyName>|</Product>)', '$1%MOD_NAME%$3' | Out-File -encoding ASCII 'src\%MOD_NAME%.csproj'"
+del src\project.csproj
