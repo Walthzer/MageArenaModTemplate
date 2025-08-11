149e9979890acfc45d71f5421e7a5aacbbd75daf
Walthzer <19926813+Walthzer@users.noreply.github.com>
2025-08-11 12:51:27 +0200

    setup script fix

--- tools/setup.bat
+++ tools/setup.bat
@@ -5,7 +5,7 @@ IF NOT EXIST "settings.bat" (
 )
 call settings.bat
 :: setup the thunderstore manifest.json
-powershell -Command "(gc 'packageFiles\manifest.json') -replace 'MOD_NAME', '%MOD_NAME%' -replace 'AUTHOR_NAME', '%AUTHOR_NAME%' | Out-File -encoding ASCII 'packageFiles\manifest.json'"
+powershell -Command "((gc 'packageFiles\manifest.json') -replace 'MOD_NAME', '%MOD_NAME%') -replace 'MOD_AUTHOR', '%MOD_AUTHOR%' | Out-File -encoding ASCII 'packageFiles\manifest.json'"
 
 ::setup the project.csproj
 powershell -Command "(gc 'src\MageArenaModTemplate.csproj') -replace '(<AssemblyName>|<Product>)(.*?)(</AssemblyName>|</Product>)', '$1%MOD_NAME%$3' | Out-File -encoding ASCII 'src\MageArenaModTemplate.csproj'"
